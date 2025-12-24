95833044587378e8f8452d0cf4dc4fb6282aa9e5
최진옥 <dkdldb5044@naver.com>
2025-12-24 16:58:42 +0900

    app.jsx 파일수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -11,20 +10,17 @@ import MovieDetailPage from './pages/MovieDetailPage';
 
 export default function App() {
   return (
-    <AuthProvider>
-      {/* GitHub Pages에서는 HashRouter가 가장 안전합니다. basename 없이 그대로 사용하세요. */}
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="signup" element={<Signup />} />
-            <Route path="mypage" element={<MyPage />} />
-            <Route path="search" element={<Search />} />
-            <Route path="movie/:id" element={<MovieDetailPage />} />
-          </Route>
-        </Routes>
-      </HashRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="mypage" element={<MyPage />} />
+          <Route path="search" element={<Search />} />
+          <Route path="movie/:id" element={<MovieDetailPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
